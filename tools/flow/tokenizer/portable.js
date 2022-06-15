798554a673d0847122c6bea26c6a441bf3216775
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2022-06-15 18:30:30 +0200

    added initial implementation of makeTokenizer for javascript

---
+++ tools/flow/tokenizer/portable.js
@@ -0,0 +1,43 @@
+export default function makeTokenizer(struct) {
+  return function tokenizer(text) {
+    let state = 0;
+    let content = '';
+    let at = 0;
+    let to = 0;
+    let tokens = [];
+    const [managers, omits] = struct;
+    for (let char of text + '\0') {
+      while (char !== null) {
+        const manager = managers[state];
+        let action = manager[1];
+        for (const handler of manager[0]) {
+          if (handler[0].includes(char)) {
+            action = handler[1];
+            break;
+          }
+        }
+        if (!action) {
+          throw new SyntaxError();
+        }
+        if (action[0]) { // add
+          content += char;
+        }
+        if (action[1]) { // use
+          to++;
+          char = null;
+        }
+        if (action[2]) { // build
+          if (!omits.includes(action[2])) {
+            let token = {type: action[2], content: content, at: at, to: to};
+            tokens.push(token)
+          }
+          content = '';
+          at = to;
+        }
+        state = action[3];
+      }
+    }
+    return tokens;
+  }
+}
+
