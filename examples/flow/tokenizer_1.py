db1544a72fd8527b6096de5c8400af6385e8f54c
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2022-06-17 16:57:11 +0200

    hotfix

--- examples/flow/tokenizer_1.py
+++ examples/flow/tokenizer_1.py
@@ -1,6 +1,7 @@
 import string
 
 from tools import files
+from tools.flow.tokenizer import *
 from tools.flow.tokenizer.port.portable import make_tokenizer_function
 
 # create the tokenizer
@@ -33,17 +34,17 @@ DIRECT_USE = False
 if __name__ == '__main__':
     # export the data to json, which can then be re-used using other languages
     files.save_json_file('tokenizer_1.json', flow.data)
-
+    
     if DIRECT_USE:
         # you can now use the created flow to tokenize some text
         tokenize = flow.__call__
-
+    
     else:
         # or if you want to use the json file create a tokenizer function from the data
-        data = tuple(files.load_json_file('tokenizer_1.json'))
+        data = files.load_json_file('tokenizer_1.json')
         tokenize = make_tokenizer_function(data)
-
+    
     src = "1 + 23 * 45 - 67 / 890"
-
+    
     for token in tokenize(src):
         print(f"{token.type.ljust(8)} -> {token.content!r}")
